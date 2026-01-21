8461811bb2be36851831f67b4d978b8d2e5b5f0b
Lizandro Santos <108939593+LizandroCloud@users.noreply.github.com>
2026-01-21 12:28:06 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = '_static/logo.png'
-html_static_path = ['_static']
+html_logo = 'static/logo.png'
+html_static_path = ['static']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
