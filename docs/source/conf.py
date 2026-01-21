91c1d17d64dc6ff4e2898f1d6e876e21c2eec140
Lizandro Santos <108939593+LizandroCloud@users.noreply.github.com>
2026-01-21 10:22:12 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
-html_logo = "_docs/static/logo.png"
-project = 'Lumache'
-copyright = '2021, Graziella'
+
+project = 'SPAIDER'
+copyright = '2026, Graziella'
 author = 'Graziella'
 
 release = '0.1'
@@ -30,6 +30,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "_static/spaider_logo.png"
+html_static_path = ['_static']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
