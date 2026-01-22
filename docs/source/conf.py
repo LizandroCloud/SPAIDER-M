3ffe747d55a450617a973adb9be473b6cbb0a32e
Lizandro Santos <108939593+LizandroCloud@users.noreply.github.com>
2026-01-22 00:07:39 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 html_theme_options = {
     'github_user': 'sphinx-doc',
     'github_repo': 'alabaster',
-    'logo_name': False,
+    "logo_name": False,
 }
 html_logo = 'static/logo_v4.png'
 html_static_path = ['static']
