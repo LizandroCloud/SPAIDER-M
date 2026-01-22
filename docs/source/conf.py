00c64dc50e48224f79b981f55a88b9336b727bb8
Lizandro Santos <108939593+LizandroCloud@users.noreply.github.com>
2026-01-22 09:30:58 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ html_theme = 'sphinx_rtd_theme'
 
 html_logo = 'static/logo_v4.png'
 html_static_path = ['static']
-
+html_title = "SPAIDER"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
